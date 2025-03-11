fec4599cd1be002125cc6a7a5fa5e5b6e815c430
Rhystalgie <aufamuh12@gmail.com>
2025-03-11 12:28:04 +0700

    menambahkan class Penjumlahan dengan method JumlahTigaAngka

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Penjumlahan
+{
+    public static double JumlahTigaAngka<T>(T a, T b, T c) where T : struct, IConvertible
+    {
+        return Convert.ToDouble(a) + Convert.ToDouble(b) + Convert.ToDouble(c);
+    }
+
+    public static void Main(string[] args)
+    {
+        double angka1 = 22.0, angka2 = 11.0, angka3 = 10.0;
+        Console.WriteLine("Hasil: " + JumlahTigaAngka(angka1, angka2, angka3));
+    }
+}
--- obj/Debug/net8.0/modul5_2211104015.AssemblyInfo.cs
+++ obj/Debug/net8.0/modul5_2211104015.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("modul5_2211104015")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1aa9a3470f73a0fe62699a1fc5b3959740fffddb")]
 [assembly: System.Reflection.AssemblyProductAttribute("modul5_2211104015")]
 [assembly: System.Reflection.AssemblyTitleAttribute("modul5_2211104015")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
