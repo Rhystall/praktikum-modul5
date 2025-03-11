8acb434963144d0098df3cdc0353fb8922c69c0a
Rhystalgie <aufamuh12@gmail.com>
2025-03-11 12:36:12 +0700

    menambahkan class SimpleDataBase

--- Program.cs
+++ Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Penjumlahan
+{
+    public static double JumlahTigaAngka<T>(T a, T b, T c) where T : struct, IConvertible
+    {
+        return Convert.ToDouble(a) + Convert.ToDouble(b) + Convert.ToDouble(c);
+    }
+}
+
+class SimpleDataBase<T>
+{
+    private List<T> storedData;
+    private List<DateTime> inputDates;
+
+    public SimpleDataBase()
+    {
+        storedData = new List<T>();
+        inputDates = new List<DateTime>();
+    }
+
+    public void AddNewData(T data)
+    {
+        storedData.Add(data);
+        inputDates.Add(DateTime.UtcNow);
+    }
+
+    public void PrintAllData()
+    {
+        for (int i = 0; i < storedData.Count; i++)
+        {
+            Console.WriteLine($"Data {i + 1} berisi: {storedData[i]}, yang disimpan pada waktu UTC: {inputDates[i]}");
+        }
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        double angka1 = 12.0, angka2 = 34.0, angka3 = 56.0;
+        Console.WriteLine("Hasil: " + Penjumlahan.JumlahTigaAngka(angka1, angka2, angka3));
+
+        // Menggunakan SimpleDataBase untuk menyimpan data
+        SimpleDataBase<double> database = new SimpleDataBase<double>();
+        database.AddNewData(angka1);
+        database.AddNewData(angka2);
+        database.AddNewData(angka3);
+
+        // Menampilkan semua data yang telah disimpan
+        database.PrintAllData();
+    }
+}
